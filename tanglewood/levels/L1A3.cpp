12aa3ab3095f5b391904e1d17972d998e16c3c55
DESKTOP-99MT4GR\Matt <matt@tanglewoodgame.com>
2018-10-06 11:23:02 +0100

    L1A3 initial commit

---
+++ tanglewood/levels/L1A3.cpp
@@ -0,0 +1,67 @@
+///////////////////////////////////////////////////////////////
+// (c) 2018 Matt Phillips, Big Evil Corporation
+//
+// File:		L1A3.cpp
+// Date:		1st October 2018
+// Authors:		Matt Phillips
+// Description:	Chapter 1 Harlequin Forest Act 3
+//				(loosely mirrors Mega Drive framework)
+///////////////////////////////////////////////////////////////
+
+#include "L1A3.h"
+#include "Globals.h"
+#include "framework/World.h"
+#include "tanglewood/Djakk.h"
+
+#include <ion/core/string/String.h>
+
+L1A3::L1A3()
+{
+	TriggerBox::RegisterTriggerFunc("L1A3_Trigger_Djakk", std::bind(&L1A3::OnTriggerDjakk, this, std::placeholders::_1));
+	TriggerBox::RegisterTriggerFunc("L1A3_Trigger_End", std::bind(&L1A3::OnTriggerEndLevel, this, std::placeholders::_1));
+}
+
+L1A3::~L1A3()
+{
+	TriggerBox::UnregisterTriggerFunc("L1A3_Trigger_Djakk");
+	TriggerBox::UnregisterTriggerFunc("L1A3_Trigger_End");
+}
+
+void L1A3::Start()
+{
+
+}
+
+void L1A3::Update(float deltaTime)
+{
+
+}
+
+void L1A3::End()
+{
+
+}
+
+void L1A3::OnTriggerDjakk(const TriggerBox& triggerBox)
+{
+	std::vector<Djakk*> djakks = Globals::Game::world->GetEntities<Djakk>();
+	
+	Djakk* djakk = nullptr;
+
+	for (int i = 0; i < djakks.size() && !djakk; i++)
+	{
+		if (ion::string::CompareNoCase(djakks[i]->m_name, "djakk"))
+		{
+			djakk = djakks[i];
+		}
+	}
+
+	ion::debug::Assert(djakk, "L1A3::OnTriggerDjakk() - Djakk not found");
+
+	djakk->BeginChase(true);
+}
+
+void L1A3::OnTriggerEndLevel(const TriggerBox& triggerBox)
+{
+	EndLevel();
+}
---
+++ tanglewood/levels/L1A3.h
@@ -0,0 +1,28 @@
+///////////////////////////////////////////////////////////////
+// (c) 2018 Matt Phillips, Big Evil Corporation
+//
+// File:		L1A3.h
+// Date:		1st October 2018
+// Authors:		Matt Phillips
+// Description:	Chapter 1 Harlequin Forest Act 2
+//				(loosely mirrors Mega Drive framework)
+///////////////////////////////////////////////////////////////
+
+#pragma once
+
+#include "framework/Level.h"
+#include "tanglewood/TriggerBox.h"
+
+class L1A3 : public Level
+{
+public:
+	L1A3();
+	virtual ~L1A3();
+
+	virtual void Start();
+	virtual void Update(float deltaTime);
+	virtual void End();
+
+	void OnTriggerDjakk(const TriggerBox& triggerBox);
+	void OnTriggerEndLevel(const TriggerBox& triggerBox);
+};
